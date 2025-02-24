b8bf74e6e891736b1fd3f1687c52836948ebc4d8
Marc Fouché <marc.fouche@happn.fr>
2025-02-24 12:43:29 +0100

    Update flake files

--- flake.nix
+++ flake.nix
@@ -1,10 +1,9 @@
 {
-  description = "Application packaged using poetry2nix";
+  description = "CLI tool to run commands on ES clusters packaged with poetry2nix";
 
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     flake-utils.url = "github:numtide/flake-utils";
-    # Last working commit from nixos-small-unstable
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     poetry2nix = {
       url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,58 +11,50 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
-        esctl = { poetry2nix, lib }: poetry2nix.mkPoetryApplication {
-          preferWheels = true;
-          projectDir = self;
-          overrides = poetry2nix.overrides.withDefaults (final: super:
-            lib.mapAttrs
-              (attr: systems: super.${attr}.overridePythonAttrs
-                (old: {
-                  nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ map (a: final.${a}) systems;
-                }))
-              {
-                # https://github.com/nix-community/poetry2nix/blob/master/docs/edgecases.md#modulenotfounderror-no-module-named-packagename
-                # package = [ "setuptools" ];
-              }
-          );
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    poetry2nix,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [
-            poetry2nix.overlays.default
-            (final: _: {
-              esctl = final.callPackage esctl { };
-            })
-          ];
         };
-      in
-      {
-        packages.default = pkgs.esctl;
-        devShells = {
-          # Shell for app dependencies.
-          #
-          #     nix develop
-          #
-          # Use this shell for developing your app.
-          default = pkgs.mkShell {
-            inputsFrom = [ pkgs.esctl ];
-            packages = [ pkgs.esctl ];
-          };
+        # create a custom "mkPoetryApplication" API function that under the hood uses
+        # the packages and versions (python3, poetry etc.) from our pinned nixpkgs above:
+        inherit (poetry2nix.lib.mkPoetry2Nix {inherit pkgs;}) mkPoetryApplication;
+        esctl = mkPoetryApplication {
+          projectDir = ./.;
+          preferWheels = true;
+        };
+      in {
+        # Used for `nix shell` and `nix profile install`
+        packages.default = esctl;
 
-          # Shell for poetry.
-          #
-          #     nix develop .#poetry
-          #
-          # Use this shell for changes to pyproject.toml and poetry.lock.
+        # Used for `nix run`
+        apps.default = {
+          type = "app";
+          program = "${esctl}/bin/esctl";
+        };
+
+        devShells = {
+          # Development shell with esctl available and all dependencies
+          # Used with `nix develop`
+          default = pkgs.mkShell {
+            inputsFrom = [esctl];
+            packages = [esctl];
+          };
+          # Development shell with only poetry available
+          # Used with `nix develop .#poetry`
           poetry = pkgs.mkShell {
-            packages = [ pkgs.poetry ];
+            packages = [pkgs.poetry];
           };
         };
-        legacyPackages = pkgs;
+
+        # Used for `nix fmt`
+        formatter = pkgs.alejandra;
       }
     );
 }
