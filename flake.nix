a0503ccb737099f1140ffd01a58c1588062c3392
Marc Fouché <marc.fouche@happn.fr>
2025-10-13 16:18:01 +0200

    Do not use poetry2nix to build esctl.
    Poetry2nix is not maintained anymore. We use a more native approach to
    build esctl. This requires to "downgrade" the requirements of some
    libraries.

--- flake.nix
+++ flake.nix
@@ -1,59 +1,116 @@
 {
-  description = "CLI tool to run commands on ES clusters packaged with poetry2nix";
+  description = "Esctl - command-line utility to connect and administrate elasticsearch clusters";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     flake-utils.url = "github:numtide/flake-utils";
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-    };
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    poetry2nix,
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {
           inherit system;
         };
-        # create a custom "mkPoetryApplication" API function that under the hood uses
-        # the packages and versions (python3, poetry etc.) from our pinned nixpkgs above:
-        inherit (poetry2nix.lib.mkPoetry2Nix {inherit pkgs;}) mkPoetryApplication;
-        esctl = mkPoetryApplication {
-          projectDir = ./.;
-          preferWheels = true;
-        };
       in {
-        # Used for `nix shell` and `nix profile install`
-        packages.default = esctl;
+        packages = {
+          default = self.packages.${system}.esctl;
 
-        # Used for `nix run`
-        apps.default = {
-          type = "app";
-          program = "${esctl}/bin/esctl";
+          esctl = pkgs.python313Packages.buildPythonApplication {
+            pname = "esctl";
+            version =
+              if self ? shortRev && self.shortRev != null && self.shortRev != ""
+              then self.shortRev
+              else "dirty";
+            src = ./.;
+            format = "pyproject";
+            build-system = with pkgs.python313Packages; [
+              poetry-core
+            ];
+
+            propagatedBuildInputs = with pkgs.python313.pkgs; [
+              self.packages.${system}.yamlpath
+              rich
+              requests
+              typer
+              jmespath
+              elasticsearch
+              pydantic
+              jsonpath-ng
+              kubernetes
+              ipython
+              blake3
+              orjson
+            ];
+
+            meta = with pkgs.lib; {
+              description = "Command-line utility to connect and administrate elasticsearch clusters";
+              license = licenses.asl20;
+              platforms = platforms.all;
+            };
+          };
+
+          # ruamel-yaml is too up to date in nixpkgs, so we vendor it here for yamlpath
+          ruamel-yaml = pkgs.python313Packages.buildPythonPackage rec {
+            pname = "ruamel.yaml";
+            version = "0.17.21";
+            pyproject = true;
+            src = pkgs.fetchPypi {
+              inherit pname version;
+              sha256 = "sha256-i3zml6LyEnUqNcGsQURx3BbEJMlXO+SSa1b/P10jt68=";
+            };
+            doCheck = false;
+            propagatedBuildInputs = with pkgs.python313.pkgs; [
+              setuptools
+            ];
+            meta = with pkgs.lib; {
+              description = "ruamel.yaml is a YAML parser/emitter that supports roundtrip preservation of comments, seq/map flow style, and map key order";
+              license = licenses.mit;
+              platforms = platforms.all;
+            };
+          };
+
+          # yamlpath is not in nixpkgs, so we vendor it here
+          yamlpath = pkgs.python313Packages.buildPythonPackage rec {
+            pname = "yamlpath";
+            version = "3.8.2";
+            pyproject = true;
+            src = pkgs.fetchPypi {
+              inherit pname version;
+              sha256 = "sha256-TzDMIUtQhdSw53VuBsOvOuWJ7N6WUNKtp+HTRexP2k8=";
+            };
+            propagatedBuildInputs = with pkgs.python313.pkgs; [
+              setuptools
+              python-dateutil
+              self.packages.${system}.ruamel-yaml
+            ];
+            doCheck = false;
+            meta = with pkgs.lib; {
+              description = "Command-line get/set/merge/validate/scan/convert/diff processors for YAML/JSON/Compatible data using powerful, intuitive, command-line friendly syntax";
+              license = licenses.isc;
+              platforms = platforms.all;
+            };
+          };
         };
 
         devShells = {
-          # Development shell with esctl available and all dependencies
-          # Used with `nix develop`
+          # nix develop
           default = pkgs.mkShell {
-            inputsFrom = [esctl];
-            packages = [esctl];
-          };
-          # Development shell with only poetry available
-          # Used with `nix develop .#poetry`
-          poetry = pkgs.mkShell {
-            packages = [pkgs.poetry];
+            inputsFrom = [self.packages.${system}.esctl];
+            packages = with pkgs; [
+              poetry
+              ruff
+              python313Packages.mkdocs
+              python313Packages.pymdown-extensions
+              python313Packages.mike
+              python313Packages.mkdocs-awesome-nav
+            ];
           };
         };
-
-        # Used for `nix fmt`
         formatter = pkgs.alejandra;
       }
     );
